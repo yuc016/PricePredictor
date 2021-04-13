Fixed test trade calculation error and refactored model object instantiation code

data/kaggle/make_training_data.py
@@ -236,10 +236,22 @@ high_low_stats = candle_stats[:,1:3]
 
 # %%
 # Convert closing price to ROC
-closing_price_roc = candle_stats[:, -1]
-# closing_price_roc = (condensed_prices_tensor[1:, col_i["Close"]] - condensed_prices_tensor[:-1, col_i["Close"]]) / condensed_prices_tensor[:-1, col_i["Close"]] * 1000
-closing_price_roc = closing_price_roc.reshape(-1,1)
+# closing_price_roc = candle_stats[:, -1] + (-1) ** (candle_stats[:, -1] > 0).float() * -1e-8
+closing_price_roc = candle_stats[:, -1].reshape(-1, 1)
+
+percentile_lowest = np.percentile(closing_price_roc.numpy(), 20).item()
+percentile_lower = np.percentile(closing_price_roc.numpy(), 40).item()
+percentile_higher = np.percentile(closing_price_roc.numpy(), 60).item()
+percentile_highest = np.percentile(closing_price_roc.numpy(), 80).item()
+
+closing_price_cat = torch.empty(closing_price_roc.shape)
+closing_price_cat = (closing_price_roc <= percentile_lowest).float() * -2 + (closing_price_roc > percentile_lowest).float() * (closing_price_roc <= percentile_lower).float() * -1 + (closing_price_roc > percentile_lower).float() * (closing_price_roc <= percentile_higher).float() * 0 + (closing_price_roc > percentile_higher).float() * (closing_price_roc <= percentile_highest).float() * 1 + (closing_price_roc > percentile_highest).float() * 2
+
+# closing_price_roc_sign = (closing_price_roc > 0).float().reshape(-1, 1) - 0.5
+# closing_price_roc_mag = torch.log(torch.abs(closing_price_roc)).reshape(-1, 1)
 assert(torch.sum(closing_price_roc != closing_price_roc) == 0)
+# assert(torch.sum(closing_price_roc_mag != closing_price_roc_mag) == 0)
+# assert(torch.sum(closing_price_roc_sign != closing_price_roc_sign) == 0)
 
 
 # %%
@@ -260,11 +272,13 @@ data_in = candle_stats
 # data_out = high_low_stats
 data_out = closing_price_roc
 # data_in = candle_stats
+# data_out = torch.cat([closing_price_cat, closing_price_roc], dim=1)
+# data_out = torch.cat([closing_price_roc_mag, closing_price_roc_sign], dim=1)
 # data_out = torch.cat([high_low_stats, closing_price_roc], dim=1)
 
 print(condensed_prices_tensor[:5])
-print(data_in[:5])
-print(data_out[:5])
+print(data_in[:25])
+print(data_out[:25])
 
 assert(data_in.shape[0] == data_out.shape[0])
 assert(data_in.shape[1] == INPUT_FEATURE_SIZE)

model.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 
-class LSTMNetV1(nn.Module):
+class PLAIN_LSTM(nn.Module):
     def __init__(self, input_feature_size, lstm_size, num_layers, output_feature_size, dropout_rate):
-        super(LSTMNetV1, self).__init__()
+        super(PLAIN_LSTM, self).__init__()
         
         self.lstm = nn.LSTM(input_feature_size, lstm_size, num_layers, batch_first=True)
 
@@ -16,10 +16,10 @@ class LSTMNetV1(nn.Module):
         
     # input_series - (Batch size x serie length x feature size)
     # output_series - (Batch size x serie length x feature size)
-    def forward(self, input_series, output_series, debug_print=False):
-        decode_serie_len = output_series.shape[1]
+    def forward(self, input_series, output_series=None, debug_print=False):
+        decode_serie_len = output_series.shape[1] if output_series is not None else 1
         if decode_serie_len > 1:
-            x = torch.cat([input_series, output_series], dim=1)
+            x = torch.cat([input_series, output_series[:, :-1, :]], dim=1)
         else:
             x = input_series
 #         print(x.shape)
@@ -33,9 +33,9 @@ class LSTMNetV1(nn.Module):
         return output_series
 
 
-class LSTMNetV2(nn.Module):
+class CNN1L_LSTM(nn.Module):
     def __init__(self, input_feature_size, conv1_size, conv1_kernel_size, lstm_size, num_layers, l2_size, output_feature_size, dropout_rate):
-        super(LSTMNetV2, self).__init__()
+        super(CNN1L_LSTM, self).__init__()
         
         self.conv1d = nn.Sequential(
             nn.Conv1d(in_channels=input_feature_size, out_channels=conv1_size, kernel_size=conv1_kernel_size, 
@@ -44,7 +44,7 @@ class LSTMNetV2(nn.Module):
             nn.ReLU()
         )
         
-        self.lstm = nn.LSTM(conv1_size, lstm_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(conv1_size + 1, lstm_size, num_layers, batch_first=True)
 
         self.lin2 = nn.Sequential(
             nn.Dropout(dropout_rate),
@@ -62,6 +62,7 @@ class LSTMNetV2(nn.Module):
 #         print(x.shape)
         x = self.conv1d(x.permute(0,2,1)).permute(0,2,1)
 #         print(x.shape)
+        x = torch.cat([x, input_series[:, :, -1].unsqueeze(2)], dim=2)
         x, _ = self.lstm(x)
 #         print(x.shape)
         x = x[:, -1:, :]
@@ -72,9 +73,9 @@ class LSTMNetV2(nn.Module):
         return output_series
     
 
-class LSTMNetV3(nn.Module):
+class LIN_CNN1L_LSTM(nn.Module):
     def __init__(self, input_feature_size, l1_size, conv1_size, lstm_size, num_layers, l2_size, output_feature_size, dropout_rate):
-        super(LSTMNetV3, self).__init__()
+        super(LIN_CNN1L_LSTM, self).__init__()
         
         self.lin1 = nn.Sequential(
                         nn.Linear(input_feature_size, l1_size),
@@ -83,12 +84,12 @@ class LSTMNetV3(nn.Module):
                     )
             
         self.conv1d = nn.Sequential(
-                    nn.Conv1d(in_channels=l1_size, out_channels=conv1_size, kernel_size=7, stride=1, padding=3, padding_mode='replicate'),
+                    nn.Conv1d(in_channels=input_feature_size, out_channels=conv1_size, kernel_size=7, stride=1, padding=3, padding_mode='replicate'),
                     nn.Dropout(dropout_rate),
                     nn.ReLU()
                 )
         
-        self.lstm = nn.LSTM(conv1_size, lstm_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(conv1_size + input_feature_size, lstm_size, num_layers, batch_first=True)
 
         self.lin2 = nn.Sequential(
             nn.Dropout(dropout_rate),
@@ -105,6 +106,7 @@ class LSTMNetV3(nn.Module):
 #         print(x.shape)
         x = self.conv1d(x.permute(0,2,1)).permute(0,2,1)
 #         print(x.shape)
+        x = torch.cat([x, input_series], dim=2)
         x, _ = self.lstm(x)
 #         print(x.shape)
         x = x[:, -1:, :]
@@ -115,22 +117,22 @@ class LSTMNetV3(nn.Module):
     
 
 
-class LSTMNetV4(nn.Module):
+class CNN2L_LSTM(nn.Module):
     def __init__(self, input_feature_size, conv1_size, conv1_kernel_size, conv2_size, conv2_kernel_size, 
                     lstm_size, num_layers, l2_size, output_feature_size, dropout_rate):
-        super(LSTMNetV4, self).__init__()
+        super(CNN2L_LSTM, self).__init__()
         
         self.conv1d = nn.Sequential(
             nn.Conv1d(in_channels=input_feature_size, out_channels=conv1_size, kernel_size=conv1_kernel_size, 
                         stride=1, padding=conv1_kernel_size//2, padding_mode='replicate'),
             nn.ReLU(),
-            nn.Conv1d(in_channels=conv1_size, out_channels=conv2_size, kernel_size=3, 
+            nn.Conv1d(in_channels=conv1_size, out_channels=conv2_size, kernel_size=conv2_kernel_size, 
                         stride=1, padding=conv2_kernel_size//2, padding_mode='replicate'),
             nn.Dropout(dropout_rate),
             nn.ReLU()
         )
         
-        self.lstm = nn.LSTM(conv2_size, lstm_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(conv2_size + input_feature_size, lstm_size, num_layers, batch_first=True)
 
         self.lin2 = nn.Sequential(
             nn.Dropout(dropout_rate),
@@ -148,6 +150,7 @@ class LSTMNetV4(nn.Module):
 #         print(x.shape)
         x = self.conv1d(x.permute(0,2,1)).permute(0,2,1)
 #         print(x.shape)
+        x = torch.cat([x, input_series], dim=2)
         x, _ = self.lstm(x)
 #         print(x.shape)
         x = x[:, -1:, :]
@@ -155,4 +158,56 @@ class LSTMNetV4(nn.Module):
         output_series = self.lin2(x)
 #         print(x.shape)
 #         exit()
+        return output_series
+
+
+class LSTM_CNN(nn.Module):
+    def __init__(self, input_feature_size, conv1_size, conv1_kernel_size, conv2_size, conv2_kernel_size, 
+                    lstm_size, num_layers, l2_size, output_feature_size, dropout_rate):
+        super(LSTM_CNN, self).__init__()
+        
+        
+        self.lstm = nn.LSTM(input_feature_size, lstm_size, num_layers, batch_first=True)
+
+        self.conv1d = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Conv1d(in_channels=lstm_size, out_channels=conv1_size, kernel_size=10, 
+                        stride=2),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=conv1_size, out_channels=conv2_size, kernel_size=6, 
+                        stride=2),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=conv2_size, out_channels=conv2_size, kernel_size=4, 
+                        stride=1),
+            nn.ReLU(),
+            nn.AvgPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(in_channels=conv2_size, out_channels=conv2_size*2, kernel_size=8, 
+                        stride=1),
+            nn.ReLU(),
+            nn.AvgPool1d(kernel_size=2, stride=2)
+        )
+
+        self.lin2 = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.ReLU(),
+            nn.Linear(conv2_size * 2, l2_size),
+            nn.Dropout(dropout_rate * 0.5),
+            nn.ReLU(),
+            nn.Linear(l2_size, output_feature_size)
+        )
+        
+    # input_series - (Batch size x serie length x feature size), [X, y]
+    # output_series - (Batch size x serie length x feature size)
+    def forward(self, input_series, debug_print=False):
+        x = input_series
+        # print(x.shape)
+        x, _ = self.lstm(x)
+        # print(x.shape)
+        x = self.conv1d(x.permute(0,2,1)).permute(0,2,1)
+        # print(x.shape)
+        x = x[:, -1:, :]
+        # print(x.shape)
+        output_series = self.lin2(x)
+        # print(x.shape)
+        # exit()
         return output_series

trainer.py
@@ -2,10 +2,10 @@ import torch
 import random
 import copy
 import fileutils
-import model
 import dataset
+import model_factory
 
-from math import sqrt
+from math import sqrt, exp
 
 
 class NeuralNetTrainer:
@@ -43,39 +43,10 @@ class NeuralNetTrainer:
     # Initialize net, optimizer, criterion and stats
     def init_experiment(self, config):
         
-        model_name = config["model"]["name"]
-        input_serie_len = config["data"]["input_serie_len"]
-        output_serie_len = config["data"]["output_serie_len"]
-        input_feature_size = config["model"]["input_feature_size"]
-        output_feature_size = config["model"]["output_feature_size"]
-        l1_size = config["model"]["l1_size"]
-        conv1_size = config["model"]["conv1_size"]
-        conv1_kernel_size = config["model"]["conv1_kernel_size"]
-        conv2_size = config["model"]["conv2_size"]
-        conv2_kernel_size = config["model"]["conv2_kernel_size"]
-        l2_size = config["model"]["l2_size"]
-        lstm_size = config["model"]["lstm_size"]
-        num_lstm_layers = config["model"]["num_lstm_layers"]
-        dropout_rate = config["model"]["dropout_rate"]
-
         learning_rate = config["training"]["learning_rate"]
         weight_decay = config["training"]["weight_decay"]
         
-        if model_name == "LSTMNetV1":
-            self.net = model.LSTMNetV1(input_feature_size, lstm_size, num_lstm_layers, output_feature_size, dropout_rate)
-        elif model_name == "LSTMNetV2":
-            self.net = model.LSTMNetV2(input_feature_size, conv1_size, conv1_kernel_size, 
-                                        lstm_size, num_lstm_layers, l2_size, output_feature_size, dropout_rate)
-        elif model_name == "LSTMNetV3":
-            self.net = model.LSTMNetV3(input_feature_size, l1_size, conv1_size, lstm_size, 
-                                        num_lstm_layers, l2_size, output_feature_size, dropout_rate)
-        elif model_name == "LSTMNetV4":
-            self.net = model.LSTMNetV4(input_feature_size, conv1_size, conv1_kernel_size, 
-                                        conv2_size, conv2_kernel_size, lstm_size, num_lstm_layers, 
-                                        l2_size, output_feature_size, dropout_rate)
-        else:
-            raise("Unknown model name", model_name)
-            
+        self.net = model_factory.build_model(config)
         self.best_net = copy.deepcopy(self.net)
         self.optimizer = torch.optim.Adam(self.net.parameters(), lr=learning_rate, weight_decay=weight_decay, eps=0)
         self.criterion = torch.nn.MSELoss(reduction="sum")
@@ -101,6 +72,7 @@ class NeuralNetTrainer:
             self.net = self.net.cuda()
             self.best_net = self.best_net.cuda()
             self.criterion = self.criterion.cuda()
+
 
     def load_data_for_training(self):
         test_set_start_index = self.config["data"]["test_set_start_index"]
@@ -264,7 +236,6 @@ class NeuralNetTrainer:
                 
         # Average by data points and data serie length, take square root to get RMSD from MSE
         test_loss = sqrt(test_loss / len(self.test_dataloader.dataset) / y.shape[1] / y.shape[2])
-        print("Test loss:", test_loss, "\n")
                 
         # Make an actual vs prediction plot
         test_sample_len = self.config["testing"]["test_regression_sample_len"]
@@ -277,9 +248,11 @@ class NeuralNetTrainer:
                                 self.experiment_dir_path)
 
         # Do a test trade
-        self.test_trade(self.config["testing"]["test_trade_start"],
-                        self.config["testing"]["test_trade_end"],
-                        actual_serie, predicted_serie, test_name)
+        product_net, my_net, trend_pred_accuracy = self.test_trade(self.config["testing"]["test_trade_start"],
+                                                                    self.config["testing"]["test_trade_end"],
+                                                                    actual_serie, predicted_serie, test_name)
+
+        fileutils.write_experiment_result(self.experiment_dir_path, test_loss, product_net, my_net, trend_pred_accuracy)
 
 
     def single_predict(self, serie):
@@ -297,25 +270,40 @@ class NeuralNetTrainer:
         if trade_start != -1 and trade_end != -1:
             actual_serie = actual_serie[trade_start:trade_end]
             predict_serie = predict_serie[trade_start:trade_end]
-        
+            
         # Start with 100 percent
-        product_net_serie = [100]
-        my_net_serie = [100]
-        product_net = 100
-        my_net = 100
+        product_net_serie = [100.0]
+        my_net_serie = [100.0]
+        product_net = 100.0
+        my_net = 100.0
         
+        BUY_TRANSACTION_FEE_RATE = 0.0002
+        SELL_TRANSACTION_FEE_RATE = 0.0002
+        BUY_THRESH = 0
         bought_in = False
+        correct_trend_pred_ct = 0
         
         for i in range(len(actual_serie)):
-            # If predict growth, buy in
-            bought_in = True if predict_serie[i,-1] > 0 else False
-            
+            # actual_roc = (-1) ** (actual_serie[i,-1] + 0.5) * -1 * exp(actual_serie[i, 0])
+            actual_roc = actual_serie[i,-1] / 1000
+            predict_roc = predict_serie[i, -1]
+
+            if actual_roc * predict_roc > 0:
+                correct_trend_pred_ct += 1
+
             # Update equity net worth
-            product_net = product_net * (1 + actual_serie[i,-1] / 1000)
-            
-            # my net worth move with actual change if bought in
-            if bought_in:
-                my_net = my_net * (1 + actual_serie[i,-1] / 1000)
+            product_net = product_net * (1 + actual_roc)
+
+            # Buy in if predict growth
+            if predict_roc > BUY_THRESH:
+                if not bought_in:
+                    my_net = my_net * (1 - BUY_TRANSACTION_FEE_RATE) # pay transaction fee
+                    bought_in = True
+                my_net = my_net * (1 + actual_roc) # value move with the product
+            else:
+                if bought_in:
+                    my_net = my_net * (1 - SELL_TRANSACTION_FEE_RATE) # pay transaction fee
+                    bought_in = False
             
             product_net_serie.append(product_net)
             my_net_serie.append(my_net)
@@ -349,13 +337,10 @@ class NeuralNetTrainer:
 
 #             product_net_serie.append(product_net)
 #             my_net_serie.append(my_net)
-            
 
-        print("Product net worth (throughout the period): {:.2f}%".format(product_net))
-        print("My net worth (trading by predicting at every time step): {:.2f}%".format(my_net))
-        print()
-        
         fileutils.make_plot([product_net_serie, my_net_serie],
                             ["Actual Product Worth", "My Net Worth"], 
                             "Time step", "Percentage of Original Capital", 
                             "test_trade"+test_name, self.experiment_dir_path)
+        
+        return product_net, my_net, correct_trend_pred_ct / len(actual_serie)